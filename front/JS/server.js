32024e6dab714244cfb6a5bc02bfa5f43254976f
phj <1004mapa2@naver.com>
2023-09-17 16:29:28 +0900

    frontFirst

---
+++ front/JS/server.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const ejs = require('ejs')
+const fs = require('fs')
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+app.use(express.static(__dirname + '/public')) //public 폴더 안에 static 파일을 넣겠다
+
+app.listen(port, function(){
+    console.log('3000 서버 열었다')
+});
+
+// mp3 파일들 이름 보내주기
+fs.readdir('public/music', (err, file) =>{
+    const fileCount = file.length;
+    const dataToSend = {
+        count: fileCount,
+        fileNameList: file
+    };
+    fetch('http://localhost:8080/saveMusicList',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(dataToSend)
+            })
+            .then(response => response.status)
+            .then(data => {
+                console.log('파일:', dataToSend);
+                console.log('파일 전송:', data);
+            })
+            .catch(error => {
+                console.error('에러 발생:', error);
+            });
+})
+
+//라우팅
+app.get('/', function(request, response){
+    // response.sendFile(__dirname + '/index.html')
+    response.render('main')
+});
+
+app.get('/quiz', function(request, response){
+    response.render('quiz')
+});
+
+app.post('/backend-endpoint', function(request, response){
+    response.render('map')
+});
