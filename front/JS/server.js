16b7e01a7ba4a80c533c65dffe5a3e6afaac7fa5
phj <1004mapa2@naver.com>
2023-09-20 18:30:19 +0900

    test11

--- front/JS/server.js
+++ front/JS/server.js
@@ -21,7 +21,7 @@ fs.readdir('public/music', (err, file) =>{
         count: fileCount,
         fileNameList: file
     };
-    fetch('http://localhost:8080/saveMusicList',{
+    fetch('http://127.0.0.1:8080/saveMusicList',{
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
